0f8cf61e9a3de3a0b0c5cbf9f92c4988460a5bb7
sid25081998 <sridharswain25@hotmail.com>
2016-09-25 17:01:20 +0530

    Space changes to learn

--- source-code/app/src/main/java/com/eventapp/pitch/Activities/BasicDataActivity.java
+++ source-code/app/src/main/java/com/eventapp/pitch/Activities/BasicDataActivity.java
@@ -81,5 +81,4 @@ public class BasicDataActivity extends Activity{
     void saveUsername() {
         SharedPreferenceMethods.setString(BasicDataActivity.this,SharedPreferenceMethods.AUTHOR_NAME,et_username.getText().toString());
     }
-
 }
